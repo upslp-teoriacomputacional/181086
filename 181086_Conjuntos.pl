538a6ee70ff4d3d358022cde8da2790e8f59722c
181086 <70725403+181086@users.noreply.github.com>
2020-09-13 23:30:18 -0500

    181086 código de programación para el tema de conjuntos

--- 181086_Conjuntos.pl
+++ 181086_Conjuntos.pl
@@ -30,20 +30,53 @@ print("el set A es: ",$A,"\n");
 print("el set B es: ",$B,"\n");
 print("el set C es: ",$C,"\n");
 
+#Pertenencia
+sub pertenencia(){
+	my $A = Set::Scalar -> new(1,2,3,4,5);
+	#1 en A
+	if($A->has(1)){
+		print("1 pertenece al conjunto A\n");
+	}else{
+		print("1 no pertenece al conjunto A\n");
+	}
+	#10 en A
+	if($A->has(10)){
+		print("10 pertenece al conjunto A\n");
+	}else{
+		print("10 no pertenece al conjunto A\n");
+	}
+}
+
+#Convertir a un conjuntos
+sub transformar(){
+	my @A = (1,2,3); #arreglo
+	my $ConjuntoA = Set::Scalar -> new(@A); #conjunto
+	print("El conjunto A es: ",$ConjuntoA,"\n");
+	my @B = (1,2,3,4,5);
+	my $ConjuntoB = Set::Scalar -> new(@B);
+	print("El conjunto B es: ",$ConjuntoB,"\n");
+	my @C="hola mundo";
+	my $ConjuntoC = Set::Scalar -> new(@C);
+	print("El conjunto C es: ",$ConjuntoC,"\n");
+}
+
 #Remover un elemento del set
 sub quitar(){
+	my $A=Set::Scalar -> new(0,1,2,3,4,5);
 	$A -> delete(2);
 	print("El set A luego de eliminar: ",$A,"\n");
 }
 
 #Remover todos los elementos de un set
 sub clear(){
+	my $A=Set::Scalar -> new(0,1,2,3,4,5);
 	$A -> clear();
 	print("El set A luego de eliminar todos sus elementos: ",$A,"\n");
 }
 
 #Copiar un set
 sub copiar(){
+	my $A=Set::Scalar -> new(0,1,2,3,4,5);
 	$B = $A->copy();
 	print("El set A: ",$A," y el set B: ",$B,"\n");
 }
@@ -58,21 +91,29 @@ sub agregar(){
 
 ##Unión
 sub union(){	
+	my $A=Set::Scalar -> new(1,2,3,4,5);
+	my $B=Set::Scalar -> new(3,4,5,6,7);
 	print("La union de A y B: ",$A->union($B),"\n");
 }
 
 ##Intersección
 sub interseccion(){	
+	my $A=Set::Scalar -> new(1,2,3,4,5);
+	my $B=Set::Scalar -> new(3,4,5,6,7);
 	print("La interseccion de A y B: ",$A->intersection($B),"\n");
 }
 
 ##Diferencia
 sub diferencia(){
+	my $A=Set::Scalar -> new(1,2,3,4,5);
+	my $B=Set::Scalar -> new(3,4,5,6,7);
 	print("La diferencia de A y B: ",$A->difference($B),"\n");
 }
 
 ##Diferencia simétrica
 sub simetrica(){
+	my $A=Set::Scalar -> new(1,2,3,4,5);
+	my $B=Set::Scalar -> new(3,4,5,6,7);
 	print("diferencia simetrica A B: ",$A->symmetric_difference($B),"\n");
 	print("diferencia simetrica B A: ",$B->symmetric_difference($A),"\n");
 	print("diferencia simetrica A C: ",$A->symmetric_difference($C),"\n");
@@ -81,11 +122,14 @@ sub simetrica(){
 
 # /------------------------------------------------------------------------------------------\
 # | Para las de subconjunto y superconjunto utilicé estructuras if para poder imprimir algo  |
-# | ya que Perl no imprime nada al realizarlo de un modo más apegado al código fuente	     |
+# | ya que Perl no imprime true ni false al realizarlo de un modo más apegado al código      |
+# | fuente, solamente imprime un 1 si es lógicamente correcto y nada si es incorrecto        |
 # \------------------------------------------------------------------------------------------/
 
 #Subconjunto
 sub subconjunto(){
+	my $A=Set::Scalar -> new(1,2,3,4,5);
+	my $B=Set::Scalar -> new(0,1,2,3,4,5,6,7,8,9);
 	if($A->is_subset($B)){
 		print("A es subconjunto de B\n");
 	}else{
@@ -95,11 +139,13 @@ sub subconjunto(){
 		print("B es subconjunto de A\n");
 	}else{
 		print("B no es subconjunto de A\n");
-	}
+	}	
 }
 
 #Superconjunto
 sub superconjunto(){
+	my $A=Set::Scalar -> new(1,2,3,4,5);
+	my $B=Set::Scalar -> new(0,1,2,3,4,5,6,7,8,9);
 	if($B->is_superset($A)){
 		print("B es superconjunto de A\n");
 	}else{
@@ -113,6 +159,8 @@ sub superconjunto(){
 }
 
 #utilizar las funciones creadas para comprobar que funcionen
+pertenencia();
+transformar();
 quitar();
 clear();
 copiar();
